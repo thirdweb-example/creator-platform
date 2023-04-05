5e40717d1916bf33622b6da2b55db6e5bfff5055
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-04-05 09:21:23 +0530

    chore: update template (#4)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import { AppProps } from "next/dist/shared/lib/router/router";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { AppProps } from "next/app";
 import "../styles/globals.css";
 
-const MyApp = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  // This is the chain your dApp will work on.
+  const activeChain = "mumbai";
+
   return (
     <ThirdwebProvider
-      desiredChainId={ChainId.Goerli}
+      activeChain={activeChain}
       authConfig={{
         authUrl: "/api/auth",
         domain: process.env.NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN as string,
